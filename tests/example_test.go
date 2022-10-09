441d51c77737678ad2cb67d031d30d2b1f7cf210
Alexander Novikov <xain4400@yandex.ru>
2022-10-09 10:37:38 +0300

    1. delete redundant comments

--- tests/example_test.go
+++ tests/example_test.go
@@ -4,14 +4,14 @@ import (
 	"goApiTests/internal/dto"
 	"goApiTests/internal/steps"
 
-	//"sync"
+	"sync"
 	"testing"
 
 	"github.com/dailymotion/allure-go"
 	"github.com/inconshreveable/log15"
 )
 
-// var lock sync.Mutex = sync.Mutex{}
+var lock sync.Mutex = sync.Mutex{}
 var mockServiceApiSteps = steps.MockServiceApiSteps{}
 var userRepositorySteps = steps.UserRepositorySteps{}
 var assertionSteps = steps.AssertionSteps{}
@@ -19,8 +19,6 @@ var assertionSteps = steps.AssertionSteps{}
 func TestDatabase(t *testing.T) {
 	t.Parallel()
 	allure.Test(t, "database test", func() {
-		//lock.Lock()
-		//defer lock.Unlock()
 		log15.Info("1s")
 		user := userRepositorySteps.FindUserById(1)
 		assertionSteps.CheckUserFirstName(t, user, "John")
@@ -31,8 +29,8 @@ func TestDatabase(t *testing.T) {
 func TestApi(t *testing.T) {
 	t.Parallel()
 	allure.Test(t, "api test", func() {
-		//lock.Lock()
-		//defer lock.Unlock()
+		lock.Lock()
+		defer lock.Unlock()
 		log15.Info("2s")
 		var userResponse dto.UserResponse
 		mockServiceApiSteps.GetMockUserStep("1", &userResponse)
@@ -44,8 +42,8 @@ func TestApi(t *testing.T) {
 func TestApi2(t *testing.T) {
 	t.Parallel()
 	allure.Test(t, "api test", func() {
-		//lock.Lock()
-		//defer lock.Unlock()
+		lock.Lock()
+		defer lock.Unlock()
 		log15.Info("3s")
 		var userResponse dto.UserResponse
 		mockServiceApiSteps.GetMockUserStep("1", &userResponse)
@@ -57,8 +55,6 @@ func TestApi2(t *testing.T) {
 func TestApi3(t *testing.T) {
 	t.Parallel()
 	allure.Test(t, "api test", func() {
-		//lock.Lock()
-		//defer lock.Unlock()
 		log15.Info("4s")
 		var userResponse dto.UserResponse
 		mockServiceApiSteps.GetMockUserStep("1", &userResponse)
@@ -70,8 +66,6 @@ func TestApi3(t *testing.T) {
 func TestApi4(t *testing.T) {
 	t.Parallel()
 	allure.Test(t, "api test", func() {
-		//lock.Lock()
-		//defer lock.Unlock()
 		log15.Info("5s")
 		var userResponse dto.UserResponse
 		mockServiceApiSteps.GetMockUserStep("1", &userResponse)
